4ab963ee2c8f4201ae6d153d8c2ccbbd54b6c0ad
Mongnhi17422 <99971773+Mongnhi17422@users.noreply.github.com>
2024-12-14 19:06:01 +0700

    done

--- effect.js
+++ effect.js
@@ -19,3 +19,22 @@ document.addEventListener("DOMContentLoaded", function () {
         });
     });
 });
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    const listItems = document.querySelectorAll("#rate-star > li");
+    listItems.forEach((item, index) => {
+      
+      const stars = item.querySelectorAll(".fa-star");
+      const starCount = 5 - index; 
+
+      stars.forEach((star, i) => {
+        if (i < starCount) {
+          star.style.color = "gold"; 
+        } else {
+          star.style.color = "#555"; 
+        }
+      });
+    });
+  });
+  
